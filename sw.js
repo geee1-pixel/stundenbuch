a3eca7cddb59fc6f3540d8f3f02cc57309d22e65
geee1-pixel <geee1@gmx.de>
2026-09-17 15:36:16 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker: macht die App offline verfügbar.
 // Bei jeder Änderung an index.html die Versionsnummer erhöhen.
-const VERSION = "stundenbuch-v19";
+const VERSION = "stundenbuch-v20";
 const DATEIEN = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
